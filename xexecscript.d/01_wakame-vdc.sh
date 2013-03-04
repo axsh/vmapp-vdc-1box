23bc5b99c062478276b4614ab40504d5af49e1c2
Masahito Yoshida <masahito@axsh.net>
2013-03-04 20:39:12 +0900

    refactor execscripts.

--- xexecscript.d/01_wakame-vdc.sh
+++ xexecscript.d/01_wakame-vdc.sh
@@ -6,11 +6,17 @@
 #
 set -e
 
+echo "doing execscript.sh: $1"
+
+chroot $1 $SHELL <<EOS
 yum clean metadata --disablerepo=* --enablerepo=wakame-vdc-rhel6
 yum update  -y     --disablerepo=* --enablerepo=wakame-vdc-rhel6
 yum install -y wakame-vdc-example-1box-full-vmapp-config
+EOS
 
+chroot $1 $SHELL <<EOS
 # add vzkernel entry
 /opt/axsh/wakame-vdc/rpmbuild/helpers/edit-grub4vz.sh add
 # edit boot order to use vzkernel as default.
 /opt/axsh/wakame-vdc/rpmbuild/helpers/edit-grub4vz.sh enable
+EOS
--- xexecscript.d/02_openvswitch.sh
+++ xexecscript.d/02_openvswitch.sh
@@ -8,23 +8,23 @@ set -e
 
 ## kmod-openvswitch-vzkernel
 
+chroot $1 $SHELL <<'EOS'
 declare arch=$(arch)
 declare basearch=
 
 case ${arch} in
-x86_64)
-  basearch=x86_64
-  ;;
-i*86)
-  basearch=i386
-  ;;
+x86_64) basearch=x86_64 ;;
+  i*86) basearch=i386   ;;
 esac
 
 rpm -ql kmod-openvswitch-vzkernel >/dev/null || yum install -y http://dlc.wakame.axsh.jp/packages/rhel/6/master/20120912124632gitff83ce0/${basearch}/kmod-openvswitch-vzkernel-1.6.1-1.el6.${arch}.rpm
+EOS
 
 ## configure edge networking
 
+chroot $1 $SHELL <<EOS
 case "${ASHIBA_ENV}" in
+
 openflow)
   /opt/axsh/wakame-vdc/rpmbuild/helpers/set-openvswitch-conf.sh
   cp -f /etc/rc.d/rc.local.openflow /etc/rc.d/rc.local
@@ -36,3 +36,5 @@ netfilter|*)
   cp -f /etc/rc.d/rc.local.netfilter /etc/rc.d/rc.local
   ;;
 esac
+
+EOS
