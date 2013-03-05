de5fddf479ba7a34910c10db2080b37c90beab42
Masahito Yoshida <masahito@axsh.net>
2013-03-05 20:16:14 +0900

    remove arch command dependency.

--- xexecscript.d/50_openvswitch-openvz.sh
+++ xexecscript.d/50_openvswitch-openvz.sh
@@ -11,14 +11,6 @@ openvz) ;;
 *) exit 0 ;;
 esac
 
-chroot $1 $SHELL <<'EOS'
-declare arch=$(arch)
-declare basearch=
-
-case ${arch} in
-x86_64) basearch=x86_64 ;;
-  i*86) basearch=i386   ;;
-esac
-
-rpm -ql kmod-openvswitch-vzkernel >/dev/null || yum install -y http://dlc.wakame.axsh.jp/packages/rhel/6/master/20120912124632gitff83ce0/${basearch}/kmod-openvswitch-vzkernel-1.6.1-1.el6.${arch}.rpm
+chroot $1 $SHELL <<EOS
+  yum install -y http://dlc.wakame.axsh.jp/packages/rhel/6/master/20120912124632gitff83ce0/${basearch}/kmod-openvswitch-vzkernel-1.6.1-1.el6.${distro_arch}.rpm
 EOS
