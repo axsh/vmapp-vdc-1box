379703096533374694a599d879017bb955771ee6
Masahito Yoshida <masahito@axsh.net>
2013-03-06 14:00:13 +0900

    deploy centos1d only.

--- prepare-vmimage.sh
+++ prepare-vmimage.sh
@@ -6,25 +6,30 @@
 #  curl, zcat, cp
 #
 set -e
-set -x
+#set -x
 
 readonly abs_dirname=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-declare vmimage_base_path=${abs_dirname}/guestroot/var/lib/wakame-vdc/images
-declare vmimage_base_uri=http://dlc.wakame.axsh.jp.s3.amazonaws.com/demo/1box/vmimage/
 
 function deploy_vmimage() {
-  local vmimage_basename=$1
+  local vmimage_basename=$1 hypervisor=$2 multi_suffix=$3
   [[ -n "${vmimage_basename}" ]] || { echo "[ERROR] invalid argument: 'vmimage_basename'" >&2; return 1; }
+  [[ -n "${hypervisor}"       ]] || { echo "[ERROR] invalid argument: 'hypervisor'" >&2; return 1; }
+  [[ -n "${multi_suffix}"     ]] || { echo "[ERROR] invalid argument: 'multi_suffix'" >&2; return 1; }
 
-  local vmimage_uri=${vmimage_base_uri}/${vmimage_basename}.gz
-  local vmimage_path=${vmimage_base_path}/${vmimage_basename}
+  local vmimage_base_uri=http://dlc.wakame.axsh.jp.s3.amazonaws.com/demo/1box/vmimage
+  local vmimage_base_path=${abs_dirname}/guestroot.${hypervisor}/var/lib/wakame-vdc/images
+
+  local vmimage_uri=${vmimage_base_uri}/${vmimage_basename}.${hypervisor}.${multi_suffix}
+  local vmimage_path=${vmimage_base_path}/${vmimage_basename}.${hypervisor}.${multi_suffix}
 
   [[ -f "${vmimage_path}" ]] || {
-    curl -L -o ${vmimage_path}.gz -R ${vmimage_uri}
-    zcat ${vmimage_path}.gz | cp --sparse=always /dev/stdin ${vmimage_path}
+    curl -fsSkL -o ${vmimage_path} -R ${vmimage_uri}
   }
 }
 
-deploy_vmimage ubuntu-lucid-kvm-md-32.raw
-deploy_vmimage lb-centos-openvz-md-64-stud.raw
-deploy_vmimage centos-6.3-openvz-md-x86_64-40G.raw.tar
+for arch in x86_64 i686; do
+  for hypervisor in kvm lxc openvz; do
+    echo ... centos-6.3.${arch} ${hypervisor} md.raw.tar.gz
+    deploy_vmimage centos-6.3.${arch} ${hypervisor} md.raw.tar.gz
+  done
+done
