5f90d965e4a963f9e84d845e0ceef4ffc40e4385
Masahito Yoshida <masahito@axsh.net>
2013-03-06 14:21:26 +0900

    prepare for hva-lxc.

--- xexecscript.d/03_wakame-vdc-hva-lxc.sh
+++ xexecscript.d/03_wakame-vdc-hva-lxc.sh
@@ -15,4 +15,7 @@ esac
 
 chroot $1 $SHELL <<EOS
   yum install -y wakame-vdc-hva-lxc-vmapp-config
+
+  echo "none                    /cgroup                 cgroup  defaults        0 0" >> /etc/fstab
+  [[ -d /var/lib/lxc ]] || mkdir /var/lib/lxc
 EOS
