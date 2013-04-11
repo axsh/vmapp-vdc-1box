0883b04a6752c014ba66d0124bf069fef3f22d47
Masahito Yoshida <masahito@axsh.net>
2013-04-11 12:55:13 +0900

    xexecscript.d: add zabbix installation.

---
+++ xexecscript.d/13_zabbix-agent.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#  yum
+#
+set -e
+
+echo "doing execscript.sh: $1"
+
+chroot $1 $SHELL <<EOS
+  zabbix_version=1.8.16
+  [[ -n "${zabbix_version}" ]] && {
+    zabbix_version="-${zabbix_version}"
+  }
+
+  yum install -y \
+     zabbix${zabbix_version} \
+     zabbix-agent${zabbix_version}
+EOS
+
+chroot $1 $SHELL <<EOS
+  chkconfig --list zabbix-agent
+ #chkconfig zabbix-agent on
+EOS
