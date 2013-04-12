63e8606179fb12634d7277e7b522956195423ab3
Masahito Yoshida <masahito@axsh.net>
2013-04-12 13:38:57 +0900

    xexecscript.d: add 00_yum-conf.sh to disable "updates" repository.

---
+++ xexecscript.d/00_yum-conf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#  yum
+#
+set -e
+
+echo "doing execscript.sh: $1"
+
+# disable updates repository
+chroot $1 $SHELL <<EOS
+  sed -i 's,\[updates\],[updates]\nenabled=0,' /etc/yum.repos.d/CentOS-Base.repo
+EOS
