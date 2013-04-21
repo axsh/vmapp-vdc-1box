695e4d85321afda10e17a8f3bc2b30f3c1bbb352
Masahito Yoshida <masahito@axsh.net>
2013-04-21 22:17:20 +0900

    add vdc.sh.

---
+++ guestroot/home/centos/vdc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#  screen
+#
+
+readonly SCREEN_NAME=vdc
+readonly VDC_LOG_PATH=/var/log/wakame-vdc
+
+function screen_it {
+  local title=$1; shift
+  local cmd="$@"
+  local NL=`echo -ne '\015'`
+
+  screen -L -r ${SCREEN_NAME} -x -X screen -t ${title}
+  screen -L -r ${SCREEN_NAME} -x -p ${title} -X stuff "${cmd}$NL"
+}
+
+screen -L -d -m -S ${SCREEN_NAME} -t vdc -c ~/.screenrc
+
+for component in collector dcmgr hva; do
+  screen_it ${component} "tail -F ${VDC_LOG_PATH}/${component}.log"
+done
+
+screen -x -r ${SCREEN_NAME}
