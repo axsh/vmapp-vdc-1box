04c0942b2e6482d8e855a5226d4c856fbee6b3c1
Masahito Yoshida <masahito@axsh.net>
2013-05-29 11:02:40 +0900

    update code layout.

--- prepare-vmimage.sh
+++ prepare-vmimage.sh
@@ -30,12 +30,10 @@ function deploy_vmimage() {
 }
 
 for arch in x86_64 i686; do
-
-for hypervisor in kvm lxc openvz; do
-  for vmimage in centos-6.4 vanilla lb-centos6-stud lbnode; do
-    echo ... ${vmimage}.${arch} ${hypervisor} md.raw.tar.gz
-    time deploy_vmimage ${vmimage}.${arch} ${hypervisor} md.raw.tar.gz
+  for hypervisor in kvm lxc openvz; do
+    for vmimage in centos-6.4 vanilla lb-centos6-stud lbnode; do
+      echo ... ${vmimage}.${arch} ${hypervisor} md.raw.tar.gz
+      time deploy_vmimage ${vmimage}.${arch} ${hypervisor} md.raw.tar.gz
+    done
   done
-done
-
 done
