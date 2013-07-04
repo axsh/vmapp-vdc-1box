0287df4fb51ff846614501e4014b6988b683f282
Masahito Yoshida <masahito@axsh.net>
2013-07-04 16:18:47 +0900

    add v13.06 agents.

---
+++ xexecscript.d/10_wakame-vdc/05_wakame-vdc-bksta.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#  yum
+#
+set -e
+
+echo "doing execscript.sh: $1"
+
+chroot $1 $SHELL <<'EOS'
+  pkg_names="
+   wakame-vdc-bksta-vmapp-config
+  "
+
+  for pkg_name in ${pkg_names}; do
+    yum search ${pkg_name} | egrep -q ${pkg_name} || continue
+    yum install -y ${pkg_name}
+  done
+EOS
