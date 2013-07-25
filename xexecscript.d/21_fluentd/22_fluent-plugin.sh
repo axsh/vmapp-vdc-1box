863da3a7558a8188302044b02e84adfe8324c4cf
Masahito Yoshida <masahito@axsh.net>
2013-07-25 22:31:52 +0900

    use td-agent-addon-wakame-vdc.

--- xexecscript.d/21_fluentd/22_fluent-plugin.sh
+++ xexecscript.d/21_fluentd/22_fluent-plugin.sh
@@ -9,5 +9,11 @@ set -e
 echo "doing execscript.sh: $1"
 
 chroot $1 $SHELL <<'EOS'
-  $(rpm -ql td-agent | grep ruby/bin/fluent-gem) install cassandra -v 0.17.0 --no-ri --no-rdoc
+  pkg_name=td-agent-addon-wakame-vdc
+
+  yum search ${pkg_name} && {
+    yum install -y ${pkg_name}
+  } || {
+    $(rpm -ql td-agent | grep ruby/bin/fluent-gem) install cassandra -v 0.17.0 --no-ri --no-rdoc
+  }
 EOS
