9c59b80b8b209937626914e7d1985fb0b63ecf31
Masahito Yoshida <masahito@axsh.net>
2013-08-28 18:42:39 +0900

    guestroto: set static baseurl to centos-base repo.

--- xexecscript.d/00_yum-conf/00_yum-conf.sh
+++ xexecscript.d/00_yum-conf/00_yum-conf.sh
@@ -9,6 +9,13 @@ set -e
 echo "doing execscript.sh: $1"
 
 # disable updates repository
+#chroot $1 $SHELL -ex <<EOS
+#  sed -i 's,\[updates\],[updates]\nenabled=0,' /etc/yum.repos.d/CentOS-Base.repo
+#EOS
+#
 chroot $1 $SHELL -ex <<EOS
-  sed -i 's,\[updates\],[updates]\nenabled=0,' /etc/yum.repos.d/CentOS-Base.repo
+  cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.saved
+  sed 's,^#baseurl=.*,baseurl=${baseurl},' /etc/yum.repos.d/CentOS-Base.repo.saved \
+   | sed -n '1,/^\[updates\]/p' \
+   | egrep -v  "^\[updates\]" > /etc/yum.repos.d/CentOS-Base.repo
 EOS
