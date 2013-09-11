b63a6ec310c293437421a65aefc83e3d1599d431
Masahito Yoshida <masahito@axsh.net>
2013-09-12 03:07:07 +0900

    define td-agent version.

--- xexecscript.d/21_fluentd/21_td-agent.sh
+++ xexecscript.d/21_fluentd/21_td-agent.sh
@@ -8,13 +8,17 @@ set -e
 
 echo "doing execscript.sh: $1"
 
-chroot $1 $SHELL -ex <<'EOS'
-  pkg_names="
-   td-agent
-  "
+chroot $1 $SHELL -ex <<EOS
+  td_agent_version=1.1.13
+  [[ -n "${td_agent_version}" ]] && {
+    td_agent_version="-${td_agent_version}"
+  }
 
-  for pkg_name in ${pkg_names}; do
-    yum search ${pkg_name} | egrep -q ${pkg_name} || continue
-    yum install -y ${pkg_name}
-  done
+  yum install -y \
+     td_agent${td_agent_version} \
+EOS
+
+chroot $1 $SHELL -ex <<EOS
+  chkconfig --list td-agent
+  chkconfig td-agent on
 EOS
