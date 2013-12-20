118e5c4309783d0cfaaf3924701c2db1ca0f8906
Masahito Yoshida <masahito@axsh.net>
2013-12-20 11:52:38 +0900

    replace zabbix-jp-release with zabbix-release.

--- xexecscript.d/20_zabbix/11_zabbix-jp-release.sh
+++ xexecscript.d/20_zabbix/11_zabbix-jp-release.sh
@@ -9,5 +9,6 @@ set -e
 echo "doing execscript.sh: $1"
 
 chroot $1 $SHELL -ex <<EOS
-  rpm -Uvh http://repo.zabbix.jp/relatedpkgs/rhel6/x86_64/zabbix-jp-release-6-6.noarch.rpm
+ #rpm -Uvh http://repo.zabbix.jp/relatedpkgs/rhel6/x86_64/zabbix-jp-release-6-6.noarch.rpm
+  rpm -Uvh http://repo.zabbix.com/zabbix/1.8/rhel/6/x86_64/zabbix-release-1.8-1.el6.noarch.rpm
 EOS
