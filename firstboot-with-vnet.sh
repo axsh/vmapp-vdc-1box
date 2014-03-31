5516e3ff7e6c747def2fb1bfdf107311c64e24c4
akry <a-yokokawa@axsh.net>
2014-03-31 20:53:30 +0900

    Added firstboot-with-vnet scripts and fixed circumstances accordingly.

---
+++ firstboot-with-vnet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+
+/var/lib/wakame-vdc/demo/rc.local
+/var/lib/openvnet/demo/rc.local
--- xexecscript.d/50_openvnet/03_openvnet_install.sh
+++ xexecscript.d/50_openvnet/03_openvnet_install.sh
@@ -9,25 +9,17 @@ esac
 
 chroot $1 $SHELL -ex <<'EOS'
   pkg_names="
-    wakame-vnet-ruby
-    wakame-vnet-common
-    wakame-vnet-vnmgr
-    wakame-vnet-vna
-    wakame-vnet-webapi
+    openvnet-ruby
+    openvnet-common
+    openvnet-vnmgr
+    openvnet-vna
+    openvnet-webapi
   "
   for pkg_name in ${pkg_names}; do
     yumdownloader ${pkg_name}
     rpm --nodeps -ivh `ls ${pkg_name}*`
   done
-EOS
 
-#  yumdownloader wakame-vnet-common
-#  yumdownloader wakame-vnet-vnmgr
-#  yumdownloader wakame-vnet-vna
-#  yumdownloader wakame-vnet-webapi
-#
-#  rpm -ivh wakame-vnet-ruby-2.0.0.247.axsh0-1.x86_64.rpm
-#  rpm --nodeps -ivh `ls wakame-vnet-common*`
-#  rpm --nodeps -ivh `ls wakame-vnet-vnmgr*`
-#  rpm --nodeps -ivh `ls wakame-vnet-vna*`
-#  rpm --nodeps -ivh `ls wakame-vnet-webapi*`
+  cd /opt/axsh/openvnet/vnctl
+  PATH=/opt/axsh/openvnet/ruby/bin:${PATH}; bundle install
+EOS
