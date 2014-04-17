c6d0bb9702ab272596ea6d02b290940b2ba1e660
akry <a-yokokawa@axsh.net>
2014-04-17 21:07:19 +0900

    Edit dcmgr.conf. Launch the vnet processes.

--- firstboot-with-vnet.sh
+++ firstboot-with-vnet.sh
@@ -5,5 +5,14 @@
 #
 set -e
 
+# enable dcell configurations in dcmgr.conf
+sed -i -e 's/^#\(.*dcell\)/\1/' /etc/wakame-vdc/dcmgr.conf
+
 /var/lib/wakame-vdc/demo/rc.local
 /var/lib/openvnet/demo/rc.local
+
+# TODO find the best solution to launch the vnet processes since they are somehow disappear after openvnet/demo/rc.local
+initctl start vnet-vnmgr
+initctl start vnet-webapi
+initctl start vnet-vna
+
