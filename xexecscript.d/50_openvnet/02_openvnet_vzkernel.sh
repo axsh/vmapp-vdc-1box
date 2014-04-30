8cf74ae95f0d57b67cc1867607bdbd0369ca1cf7
akry <a-yokokawa@axsh.net>
2014-04-30 18:56:59 +0900

    Added a kernel entry of openvz to grub.conf

--- xexecscript.d/50_openvnet/02_openvnet_vzkernel.sh
+++ xexecscript.d/50_openvnet/02_openvnet_vzkernel.sh
@@ -7,16 +7,29 @@ openvz) ;;
 *) exit 0 ;;
 esac
 
+
 chroot $1 $SHELL -ex <<'EOS'
-  yum install -y http://download.openvz.org/kernel/branches/rhel6-2.6.32/042stab084.20/vzkernel-firmware-2.6.32-042stab084.20.noarch.rpm
-  yum install -y http://download.openvz.org/kernel/branches/rhel6-2.6.32/042stab084.20/vzkernel-2.6.32-042stab084.20.x86_64.rpm
+  vzkernel_version=2.6.32-042stab084.20
 
-  sed -i -e 's/^default=[0-9]*/default=2/' /boot/grub/grub.conf
+  yum install -y http://download.openvz.org/kernel/branches/rhel6-2.6.32/042stab084.20/vzkernel-firmware-${vzkernel_version}.noarch.rpm
+  yum install -y http://download.openvz.org/kernel/branches/rhel6-2.6.32/042stab084.20/vzkernel-${vzkernel_version}.x86_64.rpm
 
-  cat <<'GCNF' >> /boot/grub/grub.conf
+  [[ -z `grep ${vzkernel_version} /mnt/boot/grub/grub.conf` ]] && {
+    cat <<'GCNF' >> /boot/grub/grub.conf
 title OpenVZ (2.6.32-042stab084.20)
         root (hd0,0)
         kernel /boot/vmlinuz-2.6.32-042stab084.20 ro root=LABEL=root rd_NO_LUKS rd_NO_LVM LANG=en_US.UTF-8 rd_NO_MD SYSFONT=latarcyrheb-sun16 crashkernel=auto  KEYBOARDTYPE=pc KEYTABLE=us rd_NO_DM
         initrd /boot/initramfs-2.6.32-042stab084.20.img
 GCNF
+  }
+
+  IFS=$'\n'
+  kernel_list=(`grep "title" /boot/grub/grub.conf`)
+
+  for i in `seq 0 $((${#kernel_list[*]}-1))`; do
+    if [[ "${kernel_list[${i}]}" =~ "${vzkernel_version}" ]]; then
+      sed -i -e "s/^default=[0-9]*/default=${i}/" /boot/grub/grub.conf
+    fi
+  done
+
 EOS
