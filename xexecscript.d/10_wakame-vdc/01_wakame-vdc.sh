635ca11386e1048d544207ab4169ba9bf2ccca1d
Masahito Yoshida <masahito@axsh.net>
2014-10-17 02:18:15 +0900

    use jaist mirror.

--- xexecscript.d/10_wakame-vdc/01_wakame-vdc.sh
+++ xexecscript.d/10_wakame-vdc/01_wakame-vdc.sh
@@ -12,10 +12,14 @@ chroot $1 $SHELL -ex <<'EOS'
   rpm -Uvh http://dlc.wakame.axsh.jp.s3-website-us-east-1.amazonaws.com/epel-release
   # workaround 2014/10/17
   #
-  # in order escape below erro
+  # in order escape below error
   # > Error: Cannot retrieve metalink for repository: epel. Please verify its path and try again
   #
-  sed -i 's,^#baseurl,baseurl,; s,^mirrorlist=,#mirrorlist=,;' /etc/yum.repos.d/epel.repo
+  sed -i \
+   -e 's,^#baseurl,baseurl,' \
+   -e 's,^mirrorlist=,#mirrorlist=,' \
+   -e 's,http://download.fedoraproject.org/pub/epel/,http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/,' \
+   /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel-testing.repo
 
   yum clean metadata --disablerepo=* --enablerepo=wakame-vdc-rhel6 --enablerepo=wakame-3rd-rhel6
   yum update  -y     --disablerepo=* --enablerepo=wakame-vdc-rhel6 --enablerepo=wakame-3rd-rhel6
