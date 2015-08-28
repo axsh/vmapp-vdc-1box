2db55ae3d1609462d8ce275279b656d6b0bc0767
Masahito Yoshida <masahito@axsh.net>
2015-08-28 17:52:36 +0900

    install openvswitch-2.4.0.

--- xexecscript.d/10_wakame-vdc/06_wakame-vdc-natbox.sh
+++ xexecscript.d/10_wakame-vdc/06_wakame-vdc-natbox.sh
@@ -9,6 +9,10 @@ set -e
 echo "doing execscript.sh: $1"
 
 chroot $1 $SHELL -ex <<'EOS'
+  yum install -y http://dlc.openvnet.axsh.jp/packages/rhel/openvswitch/6.7/openvswitch-2.4.0-1.x86_64.rpm
+EOS
+
+chroot $1 $SHELL -ex <<'EOS'
   pkg_names="
    wakame-vdc-natbox-vmapp-config
   "
